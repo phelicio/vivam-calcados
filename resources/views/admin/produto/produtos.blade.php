bdb4dadacf2c75b6d47b72ec8e43c1f851bff6fb
Phelicio <phelicio.ramalho@estagiario.ufca.edu.br>
2019-08-23 16:58:03 -0300

    Botão excluir

--- resources/views/admin/produto/produtos.blade.php
+++ resources/views/admin/produto/produtos.blade.php
@@ -11,8 +11,8 @@
                 <h3 class="box-title">{{$produto->nome}}</h3>
                 <div class="pull-right">
                     <form action="{{route('produtos.destroy', $produto->id)}}" method="POST">
-                        <input type="hidden" name="_method" value="DELETE">
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        {{ method_field('DELETE') }}
+                        {{ csrf_field() }}
                         <a class="btn btn-outline glyphicon glyphicon-pencil" style="margin-right:5px; color: #605ca8;" href="{{route('produtos.edit', $produto->id)}}"></a>      
                         <button class="btn btn-outline glyphicon glyphicon-remove" style="color:lightcoral;"></button>
                     </form>
