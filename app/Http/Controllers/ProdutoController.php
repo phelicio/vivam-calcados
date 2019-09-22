d5e49f941378964ce50087d517c144db8876503d
Phelicio Ramalho <phelicio@live.com>
2019-09-22 17:33:37 -0300

    debug

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -145,7 +145,7 @@ class ProdutoController extends Controller
     public function catalogo(Request $request){
 
         $categoria = $request->input('categoria')? $request->input('categoria'): "";
-        var_dump($request->input('categoria'));
+        var_dump($request->only('categoria'));
         if(!empty($categoria)){
             $produtos = DB::table('categoria_produto')
                             ->join('produtos', 'produtos.id', '=', 'categoria_produto.produto_id')
