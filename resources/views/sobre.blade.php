fed3b2cab89a4cb4354cce42fe29de734a216cca
SusyLobo <33572079+SusyLobo@users.noreply.github.com>
2019-10-19 19:09:54 -0300

    Editando a pagina se sobre

--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -6,10 +6,15 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-6 contact-info">
-					<h3>Get in touch</h3>
-					<p>Main Str, no 23, New York</p>
-					<p>+546 990221 123</p>
-					<p>hosting@contact.com</p>
+					<h3>Vivam Calçados</h3>
+					<p>A Vivam Calçados é sistema busca explorar o mercado de e-commerce na área da venda de calçados, área pouco explorada na região do Cariri. 
+					A princípio será focado apenas na região em questão, e futuramente será estudada a possibilidade de expansão através do Nordeste e em seguida para o restante do Brasil.<p> 
+					<p>A vivam.online tem o compromisso com a privacidade e segurança de seus clientes durante todo o processo desde a navegação até a finalização da compra pelo site.
+					Os dados cadastrais dos clientes não são vendidos, trocados ou divulgados para terceiros, exceto quando essas informações são necessárias para o processo de entrega,
+					para cobrança, ou para participação em promoções solicitadas pelos clientes. Seus dados pessoais são peça fundamental para que seu pedido chegue em segurança, na sua casa, de acordo com nosso prazo de entrega.<p>
+					<p>Para que estes dados permaneçam intactos, nós aconselhamos a não divulgação de sua senha a terceiros, mesmo a amigos e parentes, desta forma seus dados se tornam mais seguros.<p>
+</p>
+					
 					<div class="contact-social">
 						<a href="#"><i class="fa fa-pinterest"></i></a>
 						<a href="#"><i class="fa fa-facebook"></i></a>
