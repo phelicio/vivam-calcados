3014d85c3aa8e9d5c709517f95d4138c51def390
Phelicio Ramalho <phelicio@live.com>
2019-10-28 13:54:38 -0300

    tirando o dd

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -241,7 +241,6 @@ class ProdutoController extends Controller
         $produtos = $produtos->unique();
         $cores = DB::table('produtos')->select('cor as nome', 'cor_html as html')->distinct()->get();
         $tamanhos = DB::table('modelos')->select('tamanho as nome')->distinct()->get()->sortBy('nome');
-        dd($tamanhos);
         return view('produto.catalogo',[
             'produtos' => $produtos,
             'categorias' => Categoria::all(),
