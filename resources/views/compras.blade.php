adaa25b2d00c0775a166ab5d559cfcb1fac72dd7
Phelicio Ramalho <phelicio@live.com>
2019-11-05 03:53:11 -0300

    Adicionando view de documentos

--- resources/views/compras.blade.php
+++ resources/views/compras.blade.php
@@ -13,14 +13,23 @@
             </tr>
         </thead>
         <tbody>
+            @if(Auth::user()->vendas->isEmpty())
+                <tr >
+                    <td colspan="3">
+                        <h3 class="text-center m-5">Você não fez nenhuma compra ainda :(</h3>
+                        <p class="text-center m-5" >Veja nossos produtos! <a href="{{ route('produtos.catalogo') }}">clique aqui</a></p>
+                    </td>
+                </tr>
+            @else
             @foreach ($vendas as $venda)
-            <tr>
-                <td>{{ date('d/m/Y', strtotime($venda->created_at)) }}</td>
-                <td>{{ date('d/m/Y', strtotime($venda->dataEntrega)) }}</td>
-                <td>{{ $venda->getStatus() }}</td>
-                <td><a href="{{ route('pedido', $venda->id) }}"> Visualizar</a></td>
-            </tr>
+                <tr>
+                    <td>{{ date('d/m/Y', strtotime($venda->created_at)) }}</td>
+                    <td>{{ date('d/m/Y', strtotime($venda->dataEntrega)) }}</td>
+                    <td>{{ $venda->getStatus() }}</td>
+                    <td><a href="{{ route('pedido', $venda->id) }}"> Visualizar</a></td>
+                </tr>
             @endforeach
+                @endif
         </tbody>
     </table>
 </div>
